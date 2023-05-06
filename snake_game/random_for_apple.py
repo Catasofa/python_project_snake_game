5d198028280e72fc750efee095ae45005c099838
Catasofa <99603315+Catasofa@users.noreply.github.com>
2023-05-06 15:50:32 +0300

    Delete random_for_apple.py

--- snake_game/random_for_apple.py
+++
@@ -1,8 +0,0 @@
-def random_for_apple():
-    x = random.randint(0, COUNT_BLOCKS - 1)
-    y = random.randint(0, COUNT_BLOCKS - 1)
-    empty_block = Player(x, y)
-    while empty_block in player:
-        empty_block.x = random.randint(0, COUNT_BLOCKS - 1)
-        empty_block.y = random.randint(0, COUNT_BLOCKS - 1)
-    return empty_block
