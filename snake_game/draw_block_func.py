816d2fe54a74c5564bda25417303509b9bce0533
Catasofa <99603315+Catasofa@users.noreply.github.com>
2023-05-06 15:49:48 +0300

    Delete draw_block_func.py

--- snake_game/draw_block_func.py
+++
@@ -1,5 +0,0 @@
-def draw_block(color, row, column):
-    pygame.draw.rect(screen, color, [SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * (column + 1),
-                                     SIZE_BLOCK + row * SIZE_BLOCK + MARGIN * (row + 1),
-                                     SIZE_BLOCK,
-                                     SIZE_BLOCK])
