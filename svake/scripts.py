3e7881e8ffc864c10da2f41dc0c7280f91c3c6d2
Catasofa <99603315+Catasofa@users.noreply.github.com>
2023-05-06 23:44:55 +0300

    Update scripts.py

--- svake/scripts.py
+++ svake/scripts.py
@@ -7,7 +7,7 @@ from pygame import *
 
 # Настройка папки ассетов
 game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, 'snake-head.bmp')
+img_folder = os.path.join(game_folder, 'pictures_folder')
 player_start = pygame.image.load_basic('snake-head.bmp')
 tail_start = pygame.image.load_basic('snake tail.bmp')
 apple_start = pygame.image.load_basic('apple_16.bmp')
