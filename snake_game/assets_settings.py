27daec24580d0b53374c1fe7a2acef1d0850eb75
Catasofa <99603315+Catasofa@users.noreply.github.com>
2023-05-06 15:49:30 +0300

    Delete assets_settings.py

--- snake_game/assets_settings.py
+++
@@ -1,7 +0,0 @@
-# Настройка папки ассетов
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, 'snake-head.bmp')
-player_start = pygame.image.load_basic('snake-head.bmp')
-tail_start = pygame.image.load_basic('snake tail.bmp')
-apple_start = pygame.image.load_basic('apple_16.bmp')
-wall = pygame.image.load_basic('wall.bmp')
